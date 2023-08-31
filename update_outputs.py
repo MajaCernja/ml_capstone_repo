6fa919463184a522dbbfa39f4808cec81dd8cfc9
MajaCernja <maja.cernja123@gmail.com>
2023-08-31 18:39:27 +0100

    modified outputs

--- update_outputs.py
+++ update_outputs.py
@@ -2,7 +2,7 @@ import numpy as np
 from utils import add_to_npy_file
 
 
-ys = [-6.515538361770915e-06, 0.012265170460017318, -0.0492450559024272, -9.892785243638617, 237.8040385524087, -1.5951343763757544, 0.12138886755506177, 7.8589485009024]
+ys = []
 
 print(np.load('initial_data/function_3/initial_outputs.npy'))
 
